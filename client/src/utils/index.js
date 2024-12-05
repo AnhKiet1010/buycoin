fc03f4b612b10ac9695eb76b318857fb0f0f56b2
LendAHand12 <lendahand8888@gmail.com>
2024-12-05 11:02:09 +0700

    done

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,5 @@
 export const formatPrice = (amount) => {
-  return parseFloat(amount).toFixed(4);
+  return parseFloat(amount).toFixed(2);
 };
 
 export const shortenWalletAddress = (address, maxLength = 10) => {
